cdb3f24a3de6b3a1f6b5281693d58e5bd08b349c
hardeepkalsi <hardeepkalsi@yahoo.com>
2016-09-05 01:25:50 -0700

    Everything

--- ScreenGrab.py
+++ ScreenGrab.py
@@ -8,40 +8,57 @@ skip = 10
 screenArea = (screenHeight*screenWidth)/(skip*skip)
 totalColor = red = green = blue = 0
 
-screen = ImageGrab.grab()
-for y in range(0, screenHeight, skip):
-    for x in range(0, screenWidth, skip):
-        color = screen.getpixel((x, y))
-        red += color[0]
-        green += color[1]
-        blue += color[2]
-averageColor = str(red/screenArea) + "," + str(green/screenArea) +"," + str(blue/screenArea)
-print averageColor
-#ser = serial.Serial('/dev/tty.usbserial', 9600)
 serialport = "COM5"
 ser = serial.Serial(serialport, 9600)
-#while(1):
- #   val=ser.readline().decode( 'ascii' ).strip()
-  #  print (val)
 while(1):
-    ser.write(averageColor)
-    time.sleep(5)
-def visualizeColor():
+    screen = ImageGrab.grab()
+    for y in range(0, screenHeight, skip):
+        for x in range(0, screenWidth, skip):
+            color = screen.getpixel((x, y))
+            red += color[0]
+            green += color[1]
+            blue += color[2]
+    red = red/screenArea
+    green = green/screenArea
+    blue = blue/screenArea
 
-    turtle.colormode(255)
-    turtle.penup()
-    turtle.setx(-50)
-    turtle.sety(45)
-    turtle.pendown()
-    turtle.setup(100,100)
-    turtle.begin_fill()
-    turtle.right(90)
-    turtle.forward(91)
-    turtle.left(90)
-    turtle.forward(91)
-    turtle.left(90)
-    turtle.forward(91)
-    turtle.color(red/screenArea, green/screenArea, blue/screenArea)
-    turtle.end_fill()
-    turtle.done()
-visualizeColor()
+    averageColor = str(red) + "," + str(green) +"," + str(blue)
+    print averageColor
+    #ser = serial.Serial('/dev/tty.usbserial', 9600)
+
+    #while(1):
+     #   val=ser.readline().decode( 'ascii' ).strip()
+      #  print (val)
+    #while(1):
+    if(blue < 128):
+        blue = 0
+    if(green < 128):
+        green = 0
+    if(red < 128):
+        red = 0
+    ser.write(str(red)+"r")
+    #print str(red)
+    time.sleep(2)
+    ser.write(str(green)+"g")
+    time.sleep(2)
+    ser.write(str(blue)+"b")
+    time.sleep(2)
+    def visualizeColor():
+
+        turtle.colormode(255)
+        turtle.penup()
+        turtle.setx(-50)
+        turtle.sety(45)
+        turtle.pendown()
+        turtle.setup(100,100)
+        turtle.begin_fill()
+        turtle.right(90)
+        turtle.forward(91)
+        turtle.left(90)
+        turtle.forward(91)
+        turtle.left(90)
+        turtle.forward(91)
+        turtle.color(red/screenArea, green/screenArea, blue/screenArea)
+        turtle.end_fill()
+        turtle.done()
+   # visualizeColor()
